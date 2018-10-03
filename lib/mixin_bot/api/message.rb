b5289204eeef0aef65da608aa83936f43c8cd09e
an-lee <an.lee.work@gmail.com>
2018-10-03 15:39:13 +0800

    update message api

--- lib/mixin_bot/api/message.rb
+++ lib/mixin_bot/api/message.rb
@@ -1,6 +1,41 @@
 module MixinBot
   class API
     module Message
+      def list_pending_message
+        write_message('LIST_PENDING_MESSAGES', {})
+      end
+
+      def acknowledge_message_receipt(message_id)
+        params = {
+          'message_id': message_id,
+          'status': 'READ'
+        }
+        write_message('ACKNOWLEDGE_MESSAGE_RECEIPT', params)
+      end
+
+      def plain_text_message(conversation_id, recipient_id, text)
+        encoded_text = Base64.encode64 text
+
+        params = {
+          "conversation_id": conversation_id,
+          'recipient_id': recipient_id,
+          "category": "PLAIN_TEXT",
+          "status": "SENT",
+          "message_id": SecureRandom.uuid,
+          "data": encoded_text
+        }
+
+        write_message("CREATE_MESSAGE", params)
+      end
+
+      def app_card_message
+        # TODO:
+      end
+
+      def app_button_group_message
+        # TODO: 
+      end
+
       def read_message(data)
         io = StringIO.new(data.pack('c*'), 'rb')
         gzip = Zlib::GzipReader.new io
