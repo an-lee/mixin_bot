4bfdeed98e3a2d2c993406a36cd66002142776d4
wenewzhang <wenewboy@gmail.com>
2019-05-24 10:46:04 +0800

    read snapshots with private memo data

--- lib/mixin_bot/api/snapshot.rb
+++ lib/mixin_bot/api/snapshot.rb
@@ -8,14 +8,19 @@ module MixinBot
         asset = options['asset']
         order = options['order']
 
-        path = 'network/snapshots'
-        payload = {
-          limit: limit,
-          offset: offset,
-          asset: asset,
-          order: order
-        }
-        client.get(path, params: payload)
+        # path = 'network/snapshots'
+
+        reqForAuthToken = "/network/snapshots"
+        reqForAuthToken += "?limit=" + limit
+        reqForAuthToken += "&offset=" + offset
+
+
+        reqForAuthToken += "&asset=" + asset
+        reqForAuthToken += "&order=" + order
+        access_token = self.access_token('GET', reqForAuthToken, '')
+        authorization = format('Bearer %s', access_token)
+        client.get(reqForAuthToken, headers: { 'Authorization': authorization })
+        # client.get(path, params: payload)
       end
 
       def read_snapshot(snapshot_id)
