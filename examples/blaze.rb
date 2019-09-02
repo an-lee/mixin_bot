3d3042c8ec8994a8cf512d9dccf38e3e7e9a9c1a
an-lee <an.lee.work@gmail.com>
2019-09-02 19:21:18 +0800

    :sparkles: (blaze) add blaze & example

---
+++ examples/blaze.rb
@@ -0,0 +1,53 @@
+require 'mixin_bot'
+require 'base64'
+require 'yaml'
+
+CONFIG = YAML.load_file("#{File.dirname __FILE__}/config.yml")
+MixinBot.client_id = CONFIG['client_id']
+MixinBot.client_secret = CONFIG['client_secret']
+MixinBot.session_id = CONFIG['session_id']
+MixinBot.pin_token = CONFIG['pin_token']
+MixinBot.private_key = CONFIG['private_key']
+
+EM.run {
+  def start_connect
+    ws = MixinBot.blaze
+
+    ws.on :open do |event|
+      puts [Time.current, :open]
+      ws.send MixinBot.api.list_pending_message
+    end
+
+    ws.on :message do |event|
+      raw = JSON.parse MixinBot.api.read_ws_message(event.data)
+      puts [Time.current, :message, raw&.[]('action')]
+
+      data = raw['data']
+      next if data.blank?
+
+      # send receipt
+      ws.send MixinBot.api.acknowledge_message_receipt(data['message_id'])
+
+      # send reply
+      if data['category'] == 'PLAIN_TEXT'
+        reply = MixinBot.api.plain_text(
+          conversation_id: data['conversation_id'],
+          recipient_id: data['user_id'],
+          data: Base64.decode64(data['data'])
+        )
+        ws.send MixinBot.api.write_ws_message(reply)
+      end
+    end
+
+    ws.on :error do |event|
+      p [:error]
+    end
+
+    ws.on :close do |event|
+      p [Time.current, :close, event.code, event.reason]
+      start_connect
+    end
+  end
+  
+  start_connect
+}
