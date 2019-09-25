338c9ea86de47cd5bf7087a03e5b2057510350d1
an-lee <an.lee.work@gmail.com>
2019-09-25 10:39:02 +0800

    :sparkles: (snapshot) add read private snapshots

--- lib/mixin_bot/api/snapshot.rb
+++ lib/mixin_bot/api/snapshot.rb
@@ -3,7 +3,7 @@
 module MixinBot
   class API
     module Snapshot
-      def read_snapshots(options = {})
+      def read_network_snapshots(options = {})
         path = format(
           '/network/snapshots?limit=%<limit>s&offset=%<offset>s&asset=%<asset>s&order=%<order>s',
           limit: options[:limit],
@@ -11,21 +11,24 @@ module MixinBot
           asset: options[:asset],
           order: options[:order]
         )
-
-        if options[:private]
-          # TODO:
-          # read private snapshots
-          access_token = access_token('GET', path)
-          authorization = format('Bearer %<access_token>s', access_token: access_token)
-          client.get(path, headers: { 'Authorization': authorization, 'Content-length': 0 })
-        else
-          # read public snapshots as default
-          client.get(path)
-        end
+        client.get(path)
       end
 
-      def read_snapshot(snapshot_id)
-        path = format('network/snapshots/%<snapshot_id>s', snapshot_id: snapshot_id)
+      def read_snapshots(options = {})
+        path = format(
+          '/snapshots?limit=%<limit>s&offset=%<offset>s&asset=%<asset>s',
+          limit: options[:limit],
+          offset: options[:offset],
+          asset: options[:asset],
+        )
+
+        access_token = access_token('GET', path)
+        authorization = format('Bearer %<access_token>s', access_token: access_token)
+        client.get(path, headers: { 'Authorization': authorization })
+      end
+
+      def read_network_snapshot(snapshot_id)
+        path = format('/network/snapshots/%<snapshot_id>s', snapshot_id: snapshot_id)
         client.get(path)
       end
     end
--- spec/mixin_bot/api/snapshot_spec.rb
+++ spec/mixin_bot/api/snapshot_spec.rb
@@ -4,14 +4,18 @@ require 'spec_helper'
 
 describe MixinBot::API::Snapshot do
   it 'read public snapshots' do
-    res = MixinBot.api.read_snapshots(limit: 5)
+    res = MixinBot.api.read_network_snapshots(limit: 5)
     expect(res['data']&.[](0)&.[]('type')).to eq('snapshot')
   end
 
-  # TODO:
-  # not verified yet
-  # it 'read private snapshots' do
-  #   res = MixinBot.api.read_snapshots(limit: 1, private: true)
-  #   expect(res['data']&.[](0)&.[]('user_id')).not_to be_nil
-  # end
+  it 'read public snapshot' do
+    snapshot_id = '9b096575-a0fd-4af1-84eb-ef87963a762d'
+    res = MixinBot.api.read_network_snapshot(snapshot_id)
+    expect(res['data']&.[]('snapshot_id')).to eq(snapshot_id)
+  end
+
+  it 'read private snapshots' do
+    res = MixinBot.api.read_snapshots(limit: 1)
+    expect(res['data']&.[](0)&.[]('type')).to eq('transfer')
+  end
 end
