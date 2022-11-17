948dcd2662d1fba6a264b19f4a9030c58f456f51
an-lee <an.lee.work@gmail.com>
2022-11-18 05:17:11 +0800

    update mvm registry

--- lib/mvm/registry.rb
+++ lib/mvm/registry.rb
@@ -9,15 +9,48 @@ module MVM
       @registry = Eth::Contract.from_abi name: 'Registry', address: registry_address, abi: File.open(File.expand_path('./abis/registry.json', __dir__)).read
     end
 
-    def asset(asset_id)
+    def pid
+      hex = @rpc.call(@registry, 'PID').to_s(16)
+      MixinBot::Utils::UUID.new(hex:hex).unpacked
+    end
+
+    def version
+      @rpc.call @registry, 'VERSION'
+    end
+
+    def asset_from_contract(contract)
+      hex = @rpc.call(@registry, 'assets', contract).to_s(16)
+      MixinBot::Utils::UUID.new(hex:hex).unpacked
+    end
+
+    def users_from_contract(contract)
+      bytes = @rpc.call(@registry, 'users', contract).bytes
+      members = []
+      length = bytes.shift(2).reverse.pack('C*').unpack1('S*')
+      length.times do
+        members << MixinBot::Utils::UUID.new(raw: bytes.shift(16).pack('C*')).unpacked
+      end
+      threshold = bytes.shift(2).reverse.pack('C*').unpack1('S*')
+      {
+        members: members,
+        threshold: threshold
+      }.with_indifferent_access
+    end
+
+    def user_from_contract(contract)
+      group = users_from_contract contract
+      group[:members].first
+    end
+
+    def contract_from_asset(asset_id)
       @rpc.call @registry, 'contracts', asset_id.gsub('-', '').to_i(16)
     end
 
-    def user(user_id)
-      multisig [user_id], 1
+    def contract_from_user(user_id)
+      contract_from_multisig [user_id], 1
     end
 
-    def multisig(user_ids, threshold)
+    def contract_from_multisig(user_ids, threshold)
       bytes = []
       bytes += MixinBot::Utils.encode_int(user_ids.length)
       bytes += [user_ids.sort.join('').gsub('-', '')].pack('H*').bytes
--- spec/mvm/registry.rb
+++ spec/mvm/registry.rb
@@ -3,13 +3,39 @@
 require 'spec_helper'
 
 describe MVM::Registry do
+  it 'get pid' do
+    res = MVM.registry.pid
+    expect(res).to eq('bd670872-76ce-3263-b933-3aa337e212a4')
+  end
+
+  it 'get version' do
+    res = MVM.registry.version
+    expect(res).to eq(1)
+  end
+
   it 'get asset contract by asset_id' do
-    res = MVM.registry.asset 'c94ac88f-4671-3976-b60a-09064f1811e8'
+    res = MVM.registry.contract_from_asset 'c94ac88f-4671-3976-b60a-09064f1811e8'
     expect(res).to eq('0x034a771797a1c8694bc33e1aa89f51d1f828e5a4')
   end
 
   it 'get user contract by mixin_id' do
-    res = MVM.registry.user 'eb15555b-fc93-3b2c-a899-5a87d29422ec'
+    res = MVM.registry.contract_from_user 'eb15555b-fc93-3b2c-a899-5a87d29422ec'
     expect(res).to eq('0x4d16716bfe976a9c5c31b7c84bc89757edf7b823')
+  end
+
+  it 'get asset id by contract' do
+    res = MVM.registry.asset_from_contract '0x034a771797a1c8694bc33e1aa89f51d1f828e5a4'
+    expect(res).to eq('c94ac88f-4671-3976-b60a-09064f1811e8')
+  end
+
+  it 'get user ids by contract' do
+    res = MVM.registry.users_from_contract '0x4d16716bfe976a9c5c31b7c84bc89757edf7b823'
+    expect(res['members']).to eq(['eb15555b-fc93-3b2c-a899-5a87d29422ec'])
+    expect(res['threshold']).to eq(1)
+  end
+
+  it 'get user id by contract' do
+    res = MVM.registry.user_from_contract '0x4d16716bfe976a9c5c31b7c84bc89757edf7b823'
+    expect(res).to eq('eb15555b-fc93-3b2c-a899-5a87d29422ec')
   end
 end
