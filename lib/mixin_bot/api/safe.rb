320d88865701601d8eac742a004ab01a5c80d4aa
an-lee <an.lee.work@gmail.com>
2023-12-10 16:48:41 +0800

    add safe methods

--- lib/mixin_bot/api/safe.rb
+++ lib/mixin_bot/api/safe.rb
@@ -31,6 +31,89 @@ module MixinBot
         authorization = format('Bearer %<access_token>s', access_token: access_token)
         client.get(path, headers: { 'Authorization': authorization })
       end
+
+      def safe_deposit_entries(chain_id, members, threshold = 1, **options)
+        path = '/safe/deposit/entries'
+        members = [members] if members.is_a? String
+        p members
+
+        payload = {
+          members: members,
+          threshold: threshold,
+          chain_id: chain_id
+        }
+
+        access_token = options[:access_token] || access_token('POST', path, payload.to_json)
+        authorization = format('Bearer %<access_token>s', access_token: access_token)
+        client.post(path, headers: { 'Authorization': authorization }, json: payload)
+      end
+
+      def safe_outputs(**kwargs)
+        limit = kwargs[:limit] || 500
+        offset = kwargs[:offset] || ''
+        state = kwargs[:state] || ''
+        threshold = kwargs[:threshold] || ''
+        access_token = kwargs[:access_token]
+        order = kwargs[:order] || 'ASC'
+        members = kwargs[:members] || []
+        members = SHA3::Digest::SHA256.hexdigest(members&.sort&.join)
+
+        path = format(
+          '/safe/outputs?limit=%<limit>s&offset=%<offset>s&state=%<state>s&members=%<members>s&threshold=%<threshold>s&order=%<order>s',
+          limit: limit,
+          offset: offset,
+          state: state,
+          members: members,
+          threshold: threshold
+        )
+        access_token ||= access_token('GET', path, '')
+        authorization = format('Bearer %<access_token>s', access_token: access_token)
+        client.get(path, headers: { 'Authorization': authorization })
+      end
+
+      def create_safe_key(**kwargs)
+        path = '/safe/keys'
+        payload = {
+          receivers: kwargs[:receivers],
+          index: kwargs[:index],
+          hint: kwargs[:hint]
+        }
+        access_token ||= access_token('POST', path, payload.to_json)
+        authorization = format('Bearer %<access_token>s', access_token: access_token)
+        client.post(path, headers: { 'Authorization': authorization }, json: payload)
+      end
+
+      def create_safe_transaction_request(request_id, raw)
+        path = '/safe/transaction/requests'
+        payload = [{
+          request_id: request_id,
+          raw: raw
+        }]
+
+        access_token = access_token('POST', path, payload.to_json)
+        authorization = format('Bearer %<access_token>s', access_token: access_token)
+        client.post(path, headers: { 'Authorization': authorization }, json: payload)
+      end
+
+      def create_safe_transaction(request_id, raw)
+        path = '/safe/transactions'
+        payload = [{
+          request_id: request_id,
+          raw: raw
+        }]
+
+        access_token = access_token('POST', path, payload.to_json)
+        authorization = format('Bearer %<access_token>s', access_token: access_token)
+        client.post(path, headers: { 'Authorization': authorization }, json: payload)
+      end
+
+      def safe_transaction(request_id)
+        path = format('/safe/transactions/%<request_id>s', request_id: request_id)
+
+        access_token = access_token('GET', path, '')
+        authorization = format('Bearer %<access_token>s', access_token: access_token)
+        client.get(path, headers: { 'Authorization': authorization })
+      end
     end
   end
 end
